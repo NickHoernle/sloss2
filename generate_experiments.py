a01b6e619a6b5a8a866e723d598938beda476aea
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-07-20 11:11:19 +0100

    update the weight of the logic loss term that is learnt by the flow

--- generate_experiments.py
+++ generate_experiments.py
@@ -16,10 +16,12 @@ base_call = (f"python main.py --dataset CIFAR100 --save {DATA_HOME}/logs/resnet_
 repeats = 1
 sloss = [True]
 sloss_weights = [1e1, 1, 1e-1, 1e-2, 1e-3]
+sloss2_weights = [1e1, 1, 1e-1]
 
 settings = [(sloss_, sloss_weight, rep)
             for sloss_ in sloss
             for sloss_weight in sloss_weights
+            for sloss2_weight in sloss2_weights
             for rep in range(repeats)]
 
 nr_expts = len(settings)
@@ -31,13 +33,14 @@ print(f'Estimated time = {(nr_expts / nr_servers * avg_expt_time)/60} hrs')
 
 output_file = open("experiment.txt", "w")
 
-for (sloss_, sloss_weight, rep) in settings:
+for (sloss_, sloss_weight, sloss2_weight, rep) in settings:
     # Note that we don't set a seed for rep - a seed is selected at random
     # and recorded in the output data by the python script
     expt_call = (
         f"{base_call} "
         f"--sloss {sloss_} "
         f"--sloss_weight {sloss_weight} "
+        f"--unl_weight {sloss2_weight} "
     )
     print(expt_call, file=output_file)
 
