5c06f98cdd76fa0ab32598df7a4645311b657e6c
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-08-07 07:34:12 +0100

    change how lr is decayed

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ parser.add_argument('--lr', default=0.1, type=float)
 parser.add_argument('--epochs', default=400, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--weight_decay', default=0.0005, type=float)
-parser.add_argument('--epoch_step', default='[50,100,150,250]', type=str,
+parser.add_argument('--epoch_step', default='[2, 50, 100, 150]', type=str,
                     help='json list with epochs to drop lr on')
 parser.add_argument('--lr_decay_ratio', default=0.2, type=float)
 parser.add_argument('--resume', default='', type=str)
