2631c632f1485aa1d18d42dfb0824c1438011140
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-09-13 14:17:16 +0100

    and update experiment files

--- generate_experiments.py
+++ generate_experiments.py
@@ -12,19 +12,17 @@ SCRATCH_HOME = f'{SCRATCH_DISK}/{USER}'
 DATA_HOME = f'{SCRATCH_HOME}/vaelib'
 base_call = (f"python main.py --save {DATA_HOME}/logs/resnet_$RANDOM$RANDOM "
              f"--depth 28 --width 2 --ngpu 1 --dataroot {DATA_HOME}/data "
-             f"--starter_counter 10 --num_labelled 4000 --cuda")
+             f"--num_labelled 4000 --cuda --ssl --min_entropy")
 
 repeats = 1
 
-sloss = [True, False]
 dataset = ["CIFAR10"]
-learning_rate = [1.0, 5e-1, 2e-1, 1e-1]
-unl_weight = [1.]
-lr_decay_ratio = [.2]
+learning_rate = [1.0, 5e-1, 1e-1, 5-2]
+unl_weight = [0.5, 0.1]
+lr_decay_ratio = [0.2, 0.1]
 
-settings = [(lr, sloss_, unl_, lr_decay_ratio_, dataset_, rep)
+settings = [(lr, unl_, lr_decay_ratio_, dataset_, rep)
             for lr in learning_rate
-            for sloss_ in sloss
             for unl_ in unl_weight
             for lr_decay_ratio_ in lr_decay_ratio
             for dataset_ in dataset
@@ -39,13 +37,12 @@ print(f'Estimated time = {(nr_expts / nr_servers * avg_expt_time)/60} hrs')
 
 output_file = open("experiment.txt", "w")
 
-for (lr, sloss_, unl_, lr_decay_ratio_, dataset_, rep) in settings:
+for (lr, unl_, lr_decay_ratio_, dataset_, rep) in settings:
     # Note that we don't set a seed for rep - a seed is selected at random
     # and recorded in the output data by the python script
     expt_call = (
         f"{base_call} "
         f"--lr {lr} "
-        f"--sloss {sloss_} "
         f"--unl_weight {unl_} "
         f"--lr_decay_ratio {lr_decay_ratio_} "
         f"--dataset {dataset_} "
