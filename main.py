00a1927b469f9f7e7aa369a0edcad973525a1fdd
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-09-22 10:06:11 +0100

    change min ent condition

--- main.py
+++ main.py
@@ -386,12 +386,13 @@ def main():
 
                 if counter > 30:
                     y_u_full, latent_u = model_y(y_u)
+                    preds = F.sigmoid(y_u_full)
                     q_mu_u, q_logvar_u, alpha_u = latent
                     KLD_u = 0.5 * (
                                 torch.sum((1 / sigma_prior) * q_logvar_u.exp() + q_mu_u.pow(2) / sigma_prior - 1 - q_logvar_u,
                                           dim=1) + num_classes * np.log(sigma_prior))
                     # KLD_u = -0.5 * torch.sum(1 + q_logvar_u - q_mu_u.pow(2) - q_logvar_u.exp())
-                    loss_u = -(alpha_u.exp()*y_u_full).sum(dim=1).mean() + weight*KLD_u.mean()
+                    loss_u = -(preds*y_u_full).sum(dim=1).mean() + weight*KLD_u.mean()
 
                     loss += args.unl_weight*loss_u
 
