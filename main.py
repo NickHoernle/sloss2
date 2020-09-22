b4355adc4bcfe4fb01c60ae6aa1577f07ce572bc
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-09-22 09:35:51 +0100

    just empower decoder some more

--- main.py
+++ main.py
@@ -186,11 +186,11 @@ class DecoderModel(nn.Module):
         self.q_logvar = nn.Sequential(nn.ReLU(), nn.Linear(num_classes, num_classes))
 
         self.net = nn.Sequential(
-            nn.Linear(num_classes, 50),
+            nn.Linear(num_classes, 100),
             nn.ReLU(),
-            nn.Linear(50, 50),
+            nn.Linear(100, 100),
             nn.ReLU(),
-            nn.Linear(50, num_classes)
+            nn.Linear(100, num_classes)
         )
 
         self.nc = num_classes
@@ -207,7 +207,7 @@ class DecoderModel(nn.Module):
         z = reparameterise(q_mu, q_logvar)
         alpha = torch.log_softmax(z, dim=1)
 
-        output = self.net(alpha)+alpha
+        output = self.net(alpha)
         return output, (q_mu, q_logvar, alpha)
 
 
@@ -318,7 +318,7 @@ def main():
 
     def compute_loss(sample):
 
-        alpha = 1./num_classes**2
+        alpha = 1./num_classes
         # mu_prior = (np.log(alpha) - 1 / np.log(alpha)) * num_classes ** 2
         sigma_prior = (1. / alpha * (1 - 2. / num_classes) + 1 / (num_classes ** 2) * num_classes / alpha)
 
@@ -380,7 +380,7 @@ def main():
                 recon_loss = F.binary_cross_entropy_with_logits(y_l_full, targets, reduction="none").sum(dim=-1)
                 loss = recon_loss.mean()
 
-                KLD = 0.5*(torch.sum((1/sigma_prior)*q_logvar.exp() + q_mu.pow(2)/sigma_prior - 1 - q_logvar, dim=1) + num_classes*torch.log(sigma_prior))
+                KLD = 0.5*(torch.sum((1/sigma_prior)*q_logvar.exp() + q_mu.pow(2)/sigma_prior - 1 - q_logvar, dim=1) + num_classes*np.log(sigma_prior))
                 # KLD = -0.5 * torch.sum(1 + q_logvar - q_mu.pow(2) - q_logvar.exp())
                 loss += weight*KLD.mean()
 
@@ -389,7 +389,7 @@ def main():
                     q_mu_u, q_logvar_u, alpha_u = latent
                     KLD_u = 0.5 * (
                                 torch.sum((1 / sigma_prior) * q_logvar_u.exp() + q_mu_u.pow(2) / sigma_prior - 1 - q_logvar_u,
-                                          dim=1) + num_classes * torch.log(sigma_prior))
+                                          dim=1) + num_classes * np.log(sigma_prior))
                     # KLD_u = -0.5 * torch.sum(1 + q_logvar_u - q_mu_u.pow(2) - q_logvar_u.exp())
                     loss_u = -(alpha_u.exp()*y_u_full).sum(dim=1).mean() + weight*KLD_u.mean()
 
