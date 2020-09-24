6c1b83c545a1fd160e785c3586568d30f3d60d0c
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-09-24 17:09:26 +0100

    try ignore reconstruction for unlabeled samples

--- main.py
+++ main.py
@@ -402,17 +402,18 @@ def main():
                     y_u_full, latent_u = model_y(y_u)
                     q_mu, q_logvar, log_alpha = latent
 
-                    ps = torch.softmax(y_u_full, dim=1)
-                    recon_loss_u = []
-                    for cat in range(num_classes):
-                        true_labels = torch.zeros_like(log_alpha)
-                        true_labels[:, cat] = 1
-                        recon_loss_u.append(F.binary_cross_entropy_with_logits(y_l_full, targets, reduction="none").sum(dim=-1))
+                    # ps = torch.softmax(y_u_full, dim=1)
+                    # recon_loss_u = []
+                    # for cat in range(num_classes):
+                    #     true_labels = torch.zeros_like(log_alpha)
+                    #     true_labels[:, cat] = 1
+                    #     recon_loss_u.append(F.binary_cross_entropy_with_logits(y_l_full, targets, reduction="none").sum(dim=-1))
 
-                    recon_loss_u = (ps*torch.stack(recon_loss_u, dim=1)).sum(dim=1).mean()
+                    # recon_loss_u = (ps*torch.stack(recon_loss_u, dim=1)).sum(dim=1).mean()
                     kl_cat = -((log_alpha.exp() * log_alpha).sum(dim=1) - np.log(num_classes)).mean()
 
-                    loss_u = recon_loss_u + kl_cat.mean()
+                    # loss_u = recon_loss_u + kl_cat.mean()
+                    loss_u = kl_cat
                     loss += args.unl_weight*loss_u
 
                 return loss, y_l_full
