3dc9b38d9dbfa9b192e2714c5f3505e7fc3d7319
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-10-02 17:21:11 +0100

    shorter experiments

--- generate_experiments.py
+++ generate_experiments.py
@@ -11,8 +11,8 @@ SCRATCH_HOME = f'{SCRATCH_DISK}/{USER}'
 
 DATA_HOME = f'{SCRATCH_HOME}/vaelib'
 base_call = (f"python main.py --save {DATA_HOME}/logs/resnet_$RANDOM$RANDOM "
-             f"--depth 28 --width 2 --ngpu 1 --dataroot {DATA_HOME}/data --epochs 300 "
-             f"--num_labelled 4000 --cuda --ssl --lp --epoch_step [10,30,50,70,90,110,130,150,170,190,210,230,250,270,290]")
+             f"--depth 28 --width 2 --ngpu 1 --dataroot {DATA_HOME}/data "
+             f"--num_labelled 4000 --cuda --ssl --lp --epoch_step [10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190]")
 
 repeats = 1
 
