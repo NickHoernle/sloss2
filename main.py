fe6d4b8f93f018eb242dbf212c6c3f02435034f1
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-10-06 14:53:28 +0100

    try separate steps

--- main.py
+++ main.py
@@ -189,7 +189,6 @@ class DecoderModel(nn.Module):
         super().__init__()
 
         # local params
-        self.logpis = nn.Sequential(nn.Linear(num_classes, 50), nn.LeakyReLU(.2), nn.Linear(50, num_classes))
         self.mu = nn.Sequential(nn.Linear(num_classes, 50), nn.LeakyReLU(.2), nn.Linear(50, z_dim))
         self.logvar = nn.Sequential(nn.Linear(num_classes, 50), nn.LeakyReLU(.2), nn.Linear(50, z_dim))
 
@@ -306,7 +305,7 @@ def main():
     def create_optimizer(args, lr):
         print('creating optimizer with lr = ', lr)
         params_ = [v for v in params.values() if v.requires_grad]
-        params_ += model_y.parameters()
+        params_ += model_y.get_local_params()
         return SGD(params_, lr, momentum=0.9, weight_decay=args.weight_decay)
 
     optimizer = create_optimizer(args, args.lr)
@@ -402,28 +401,27 @@ def main():
                     loss += semantic_loss
 
             elif args.lp:
-                weight = np.min([1., np.max([0, 0.05 * (counter)])])
+                weight = np.min([1., np.max([0, 0.05 * (counter + 1)])])
                 # weight = 1.
                 alpha = 0.001
 
                 ixs = np.arange(len(y_l))
 
-                # log_pis, (zs, mu, logvar, cluster_mus) = model_y(y_l)
-                # kld = (-0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp(), dim=-1))
-                # pred_loss = F.cross_entropy(log_pis, targets_l)
-                # loss = pred_loss + weight*kld.mean()
-
-                log_preds, latent = model_y.forward_global(y_l)
+                log_preds, latent = model_y.forward_global(y_l.detach())
                 loss = F.cross_entropy(log_preds[ixs, targets_l], targets_l)
                 zs, mu_, logvar_, cluster_mu_, cluseter_logvar_ = latent
                 kld = (-0.5 * torch.sum(1 + cluseter_logvar_ - cluster_mu_.pow(2) - cluseter_logvar_.exp(), dim=-1)).sum()/len(y_l)
                 loss += kld
+                optimizer_y.zero_grad()
+                loss.backward()
+                optimizer_y.step()
 
                 # encoder loss
+                log_preds, latent = model_y.forward_global(y_l)
                 cmu = cluster_mu_.unsqueeze(0).repeat(len(y_l), 1, 1)[ixs, targets_l]
                 clv = cluseter_logvar_.unsqueeze(0).repeat(len(y_l), 1, 1)[ixs, targets_l]
                 nll = weight*-log_normal(zs, cmu, clv).mean()
-                loss += nll
+                loss = nll
 
                 return loss, log_preds[ixs, targets_l]
 
