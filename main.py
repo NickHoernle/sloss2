2a8ccaaff8a40f2f571b85c80973a9e54de2c4b0
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-10-07 13:33:59 +0100

    add sloss layer

--- main.py
+++ main.py
@@ -417,10 +417,10 @@ def main():
                 loss += nll
 
                 # custom generator loss
-                log_preds, latent = model_y.train_generative_only(len(targets_l))
+                log_preds_g, latent = model_y.train_generative_only(len(targets_l))
                 for cat in range(num_classes):
                     fake_tgts = torch.ones_like(targets_l)*cat
-                    loss += F.cross_entropy(log_preds[:, cat, :], fake_tgts)
+                    loss += F.cross_entropy(log_preds_g[:, cat, :], fake_tgts)
 
                 # unsupervised part
                 if counter > 50:
