5d6e450a51415a3ce55e3736cfdc7d3bcd912044
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-10-09 09:02:35 +0100

    fix cmd bug

--- generate_experiments.py
+++ generate_experiments.py
@@ -12,7 +12,7 @@ SCRATCH_HOME = f'{SCRATCH_DISK}/{USER}'
 DATA_HOME = f'{SCRATCH_HOME}/vaelib'
 base_call = (f"python main.py --save {DATA_HOME}/logs/resnet_$RANDOM$RANDOM "
              f"--depth 28 --width 2 --ngpu 1 --dataroot {DATA_HOME}/data --epochs 250 "
-             f"--num_labelled 4000 --cuda --ssl --lp --epoch_step [60, 120, 160]")
+             f"--num_labelled 4000 --cuda --ssl --lp --epoch_step `[60, 120, 160]`")
 
 repeats = 1
 
